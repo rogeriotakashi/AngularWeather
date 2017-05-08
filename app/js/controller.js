faa0b536836ee0f3dbacfc51f3859d2aac33e7db
Rogério Takashi Hirata <rogeriotakashi@hotmail.com>
2017-05-08 15:49:37 -0300

    style.css implemented and images added

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,16 +1,18 @@
 angular.module('angularWeather',[])
 .controller('mainController',function($scope){
+	$scope.dynamicBlock = "empty-block";
+	$scope.resultText = "";
 
 	$scope.searchWeather = function(city){
 		$scope.showImage = true;
 		var json_obj = JSON.parse(getJSON("http://api.openweathermap.org/data/2.5/weather?q={"+city+"}&appid=0d9804d47ee2f434cb4ce6c4345aab5c"));
+		$scope.resultText = $scope.city;
 		console.log(json_obj.weather[0].description);
 		$scope.tempo = json_obj.weather[0].description;
 		console.log(json_obj);
 		$scope.temperaturaMin = Math.round(json_obj.main.temp_min - 273);
 		$scope.temperaturaMax = Math.round(json_obj.main.temp_max - 273);
-
-		
+		$scope.dynamicBlock = "half-empty-block";
 	}
 
 
