f0cc2426cae2926e9a554a33a9eeffc6e3f95fee
Rogério Takashi Hirata <rogeriotakashi@hotmail.com>
2017-05-09 17:16:43 -0300

    added fade effect with ngAnimate
    added fade effect with the class .fade.ng-enter and
    .fade.ng-enter-active using module ngAnimate from Angular lib

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,12 +1,13 @@
-angular.module('angularWeather',[])
+angular.module('angularWeather',['ngAnimate'])
 .controller('mainController',function($scope){
 	$scope.dynamicBlock = "empty-block";
 	$scope.resultText = "";
 
 	$scope.searchWeather = function(city){
 		$scope.showImage = true;
-		var json_obj = JSON.parse(getJSON("http://api.openweathermap.org/data/2.5/weather?q={"+city+"}&appid=0d9804d47ee2f434cb4ce6c4345aab5c"));
 		$scope.resultText = $scope.city;
+		var json_obj = JSON.parse(getJSON("http://api.openweathermap.org/data/2.5/weather?q={"+city+"}&appid=0d9804d47ee2f434cb4ce6c4345aab5c"));
+
 		console.log(json_obj.weather[0].description);
 		$scope.tempo = json_obj.weather[0].description;
 		console.log(json_obj);
